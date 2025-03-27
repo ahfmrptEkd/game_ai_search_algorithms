5db6e905a7bc8f44b77caad32df2ee27141a485f
ahfmrptEkd <tkandpf9914@gmail.com>
2025-03-28 01:25:30 +0900

    🐛 fix - Add beam config and fix inline function

--- src/algorithms/single_player/beam.h
+++ src/algorithms/single_player/beam.h
@@ -4,13 +4,19 @@
 #include "../../games/maze/maze_state.h"
 #include "../../common/game_util.h"
 
-// 빔 탐색으로 행동을 결정한다.
-int beamSearchAction(const State& state, const int beam_width, const int beam_depth);
+struct BeamConfig {
+    int beam_width = 2;
+    int beam_depth = 4;
+};
 
-inline void playGameBeam(const int seed, const int beam_width = 2, const int beam_depth = 4)
+// 빔 탐색으로 행동을 결정한다.
+int beamSearchAction(const State& state, const BeamConfig& config);
+
+// 기본 설정을 사용하는 함수
+inline void playGameBeam(const int seed)
 {
-    playGameWithStrategy(seed, [beam_width, beam_depth](const State& state) {
-        return beamSearchAction(state, beam_width, beam_depth);
+    playGameWithStrategy(seed, [](const State& state) {
+        return beamSearchAction(state, BeamConfig{});
     });
 }
 
