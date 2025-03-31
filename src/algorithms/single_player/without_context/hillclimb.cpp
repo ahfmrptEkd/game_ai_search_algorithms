6959b624a2a894bce073339f299ab5958518ccaf
ahfmrptEkd <tkandpf9914@gmail.com>
2025-04-01 01:00:11 +0900

    ✏️ chore - Fix typos

--- src/algorithms/single_player/without_context/hillclimb.cpp
+++ src/algorithms/single_player/without_context/hillclimb.cpp
@@ -21,13 +21,11 @@ AutoMazeState hillClimbPlacement(const AutoMazeState &state, int number)
         // 상태 복사
         auto next_state = now_state;
         
-        // 무작위로 캐릭터 하나 선택 (transition() 함수 대체)
         int character_id = GameUtil::mt_for_action() % GameConstants::AutoMaze::CHARACTER_N;
         int y = GameUtil::mt_for_action() % GameConstants::Board::H;
         int x = GameUtil::mt_for_action() % GameConstants::Board::W;
         next_state.setCharacter(character_id, y, x);
         
-        // 점수 계산 및 비교
         auto next_score = next_state.getScore();
         if (next_score > best_score)
         {
