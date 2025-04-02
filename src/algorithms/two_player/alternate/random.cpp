5240dc37dab588f2b21f8e358938f1c578c7d7e6
ahfmrptEkd <tkandpf9914@gmail.com>
2025-04-02 14:13:58 +0900

    ✨ feat - Implement random game for TwoMaze

--- src/algorithms/two_player/alternate/random.cpp
+++ src/algorithms/two_player/alternate/random.cpp
@@ -14,3 +14,21 @@ int randomAction(const TwoMazeState& state)
     }
     return legal_actions[GameUtil::mt_for_action() % (legal_actions.size())];
 }
+
+void playGameTwoMazeRandom(const std::function<int(const TwoMazeState&)>& action_func, const int seed)
+{
+    auto state = TwoMazeState(seed);
+    
+    while (!state.isDone()) {
+        std::cout << state.toString() << std::endl;
+        auto action = action_func(state);
+        state.progress(action);
+    }
+    
+    // 마지막 상태 출력
+    std::cout << state.toString() << std::endl;
+    
+    // 게임 종료 후 결과 출력
+    auto status = state.getWinningStatus();
+    printGameResult(status);
+}
--- src/algorithms/two_player/alternate/random.h
+++ src/algorithms/two_player/alternate/random.h
@@ -8,9 +8,13 @@
 // 무작위 행동 알고리즘 
 int randomAction(const TwoMazeState& state);
 
+void playGame(const std::function<int(const TwoMazeState&)>& action_func, const int seed);
+
 // 시드를 고정해서 게임을 무작위 알고리즘으로 진행
-inline void playGameTwoMazeRandom(const int seed) {
-    playGameWithStrategy<TwoMazeState>(seed, randomAction);
+inline void playGameRandom(const int seed) {
+    playGame(randomAction, seed);
 }
+
+void playGameTwoMazeRandom(const std::function<int(const TwoMazeState&)>& action_func, const int seed);
 
 #endif // TWOPLAYER_RANDOM_H
