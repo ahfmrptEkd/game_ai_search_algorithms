3862e8fe543a2f44146957ed51041a688a035fb8
ahfmrptEkd <tkandpf9914@gmail.com>
2025-04-02 19:40:58 +0900

    ✨ feat - Implement Pure Monte Carlo methods for TwoMaze game logic

---
+++ src/algorithms/two_player/alternate/mc.h
@@ -0,0 +1,23 @@
+#ifndef MONTECARLO_H
+#define MONTECARLO_H
+
+#include "../../../games/twomaze/twomaze_state.h"
+#include "../../../common/game_util.h"
+#include "random.h"
+namespace montecarlo {
+    // 몬테카를로 플레이아웃 - 랜덤 플레이아웃으로 게임 종료시 승패 점수 반환
+    double playout(TwoMazeState state);
+
+    // 몬테카를로 행동 선택
+    int monteCarloAction(const TwoMazeState& state, const int playout_number);
+}
+
+int monteCarloSearchAction(const TwoMazeState& state, const int playout_number);
+
+inline void playGameMonteCarlo(const int seed, int playout_number = GameConstants::TwoMaze::PLAYOUT_NUMBER) {
+    playGame([playout_number](const TwoMazeState& state) {
+        return monteCarloSearchAction(state, playout_number);
+    }, seed);
+}
+
+#endif // MONTECARLO_H
