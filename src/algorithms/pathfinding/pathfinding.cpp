10a7ec24003d09ee27970463dcc072a868ec9007
ahfmrptEkd <tkandpf9914@gmail.com>
2025-04-05 20:58:57 +0900

    ✨ feat - Add Dijkstra algorithm support in pathfinding factory

--- src/algorithms/pathfinding/pathfinding.cpp
+++ src/algorithms/pathfinding/pathfinding.cpp
@@ -1,6 +1,7 @@
 #include "pathfinding.h"
 #include "bfs.h"
 #include "dfs.h"
+#include "dijkstra.h"
 #include <stdexcept>
 
 std::unique_ptr<PathfindingAlgorithm> createPathfindingAlgorithm(PathfindingConstants::Algorithm type) {
@@ -9,7 +10,8 @@ std::unique_ptr<PathfindingAlgorithm> createPathfindingAlgorithm(PathfindingCons
             return std::make_unique<BFSAlgorithm>();
         case PathfindingConstants::Algorithm::DFS:
             return std::make_unique<DFSAlgorithm>();
-        
+        case PathfindingConstants::Algorithm::DIJKSTRA:
+            return std::make_unique<DijkstraAlgorithm>();
         default:
             throw std::invalid_argument("Unknown pathfinding algorithm type");
     }
