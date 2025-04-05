17761abf397543300d43fd1d2a19803a3cd287dd
ahfmrptEkd <tkandpf9914@gmail.com>
2025-04-05 16:47:04 +0900

    🐛 fix - Add comparison operators for Coord struct

--- src/common/coord.h
+++ src/common/coord.h
@@ -6,6 +6,14 @@ struct Coord
     int y_;
     int x_;
     Coord(const int y = 0, const int x = 0) : y_(y), x_(x) {}
+    
+    bool operator<(const Coord& other) const {
+        return (y_ < other.y_) || (y_ == other.y_ && x_ < other.x_);
+    }
+    
+    bool operator==(const Coord& other) const {
+        return y_ == other.y_ && x_ == other.x_;
+    }
 };
 
 #endif // COORD_H
