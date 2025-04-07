f3e31195e1e54da1cb4f5112d43cda2d942eecc7
ahfmrptEkd <tkandpf9914@gmail.com>
2025-04-07 16:40:30 +0900

    ✨ feat - Add WallMazeBitSetState class for efficient state representation

---
+++ src/games/wallmaze/wallmaze_bitset_state.h
@@ -0,0 +1,41 @@
+#ifndef WALLMAZE_BITSET_STATE_H
+#define WALLMAZE_BITSET_STATE_H
+
+#include "wallmaze_bitset.h"
+#include "wallmaze_state.h"
+#include "zobrist_hash.h"
+#include "../../common/game_constants.h"
+#include "../../common/coord.h"
+
+using single_bitset::Mat;
+
+class WallMazeBitSetState {
+private:
+    int points_[GameConstants::Board::H][GameConstants::Board::W];
+    Mat whoe_point_mat_ = Mat();
+    int turn_ = 0;
+    Mat walls_ = Mat();
+    Coord characters_ = Coord();
+
+    int getDistanceToNearestPoint();
+    void init_hash();
+
+public:
+    int game_score_ = 0;
+    GameConstants::ScoreType evaluated_score_ = 0;
+    int first_action_ = -1;
+    uint64_t hash_ = 0;
+    
+    WallMazeBitSetState() = default;
+    explicit WallMazeBitSetState(const WallMazeState &state);
+    
+    void evaluateScore();
+    bool isDone() const;
+    void advance(const int action);
+    std::vector<int> legalActions() const;
+    std::string toString() const;
+    
+    bool operator<(const WallMazeBitSetState& other) const;
+};
+
+#endif // WALLMAZE_BITSET_STATE_H
