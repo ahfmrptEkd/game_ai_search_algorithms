ba04d1694a6820cfb9057f20fc33c05e7a42c86c
ahfmrptEkd <tkandpf9914@gmail.com>
2025-04-07 21:11:53 +0900

    ✨ feat - Implement ConnectFourMCTSAlgorithm for Monte Carlo Tree Search

--- src/algorithms/algorithm_factory.cpp
+++ src/algorithms/algorithm_factory.cpp
@@ -647,6 +647,164 @@ public:
     }
 };
 
+// ConnectFour MCTS 알고리즘 클래스
+class ConnectFourMCTSAlgorithm : public Algorithm {
+private:
+    AlgorithmParams params_;
+    
+    // MCTS 구현에 필요한 상수 및 유틸리티
+    static constexpr const double C = 1.;
+    static constexpr const int EXPAND_THRESHOLD = 10;
+    
+    double playout(ConnectFourState* state) {
+        switch (state->getWinningStatus()) {
+            case (WinningStatus::WIN):
+                return 1.;
+            case (WinningStatus::LOSE):
+                return 0.;
+            case (WinningStatus::DRAW):
+                return 0.5;
+            default:
+                auto legal_actions = state->legalActions();
+                state->progress(legal_actions[GameUtil::mt_for_action() % legal_actions.size()]);
+                return 1. - playout(state);
+        }
+    }
+    
+    // MCTS 노드 클래스
+    class Node {
+    private:
+        ConnectFourState state_;
+        double w_;
+        
+    public:
+        std::vector<Node> child_nodes_;
+        double n_;
+        
+        Node(const ConnectFourState& state) : state_(state), w_(0), n_(0) {}
+        
+        double evaluate() {
+            if (this->state_.isDone()) {
+                double value = 0.5;
+                switch (this->state_.getWinningStatus()) {
+                    case (WinningStatus::WIN):
+                        value = 1.;
+                        break;
+                    case (WinningStatus::LOSE):
+                        value = 0.;
+                        break;
+                    default:
+                        break;
+                }
+                this->w_ += value;
+                ++this->n_;
+                return value;
+            }
+            
+            if (this->child_nodes_.empty()) {
+                ConnectFourState state_copy = this->state_;
+                double value = playout(&state_copy);
+                this->w_ += value;
+                ++this->n_;
+                
+                if (this->n_ == EXPAND_THRESHOLD)
+                    this->expand();
+                    
+                return value;
+            }
+            else {
+                double value = 1. - this->nextChildNode().evaluate();
+                this->w_ += value;
+                ++this->n_;
+                return value;
+            }
+        }
+        
+        void expand() {
+            auto legal_actions = this->state_.legalActions();
+            this->child_nodes_.clear();
+            for (const auto action : legal_actions) {
+                this->child_nodes_.emplace_back(this->state_);
+                this->child_nodes_.back().state_.progress(action);
+            }
+        }
+        
+        Node& nextChildNode() {
+            for (auto& child_node : this->child_nodes_) {
+                if (child_node.n_ == 0)
+                    return child_node;
+            }
+            
+            double t = 0;
+            for (const auto& child_node : this->child_nodes_) {
+                t += child_node.n_;
+            }
+            
+            double best_value = -GameConstants::INF;
+            int best_action_index = -1;
+            
+            for (int i = 0; i < this->child_nodes_.size(); i++) {
+                const auto& child_node = this->child_nodes_[i];
+                double ucb1_value = 1. - child_node.w_ / child_node.n_ + 
+                                  C * std::sqrt(2. * std::log(t) / child_node.n_);
+                                  
+                if (ucb1_value > best_value) {
+                    best_action_index = i;
+                    best_value = ucb1_value;
+                }
+            }
+            
+            return this->child_nodes_[best_action_index];
+        }
+    };
+    
+public:
+    int selectAction(const GameState& state) override {
+        auto& connectfour_state = static_cast<const ConnectFourState&>(state);
+        
+        Node root_node = Node(connectfour_state);
+        root_node.expand();
+        
+        TimeKeeper time_keeper(params_.timeThreshold);
+        
+        while (!time_keeper.isTimeOver()) {
+            root_node.evaluate();
+        }
+        
+        auto legal_actions = connectfour_state.legalActions();
+        if (legal_actions.empty()) {
+            return -1;
+        }
+        
+        int best_action_searched_number = -1;
+        int best_action_index = -1;
+        
+        for (int i = 0; i < legal_actions.size(); i++) {
+            int n = root_node.child_nodes_[i].n_;
+            if (n > best_action_searched_number) {
+                best_action_index = i;
+                best_action_searched_number = n;
+            }
+        }
+        
+        return legal_actions[best_action_index];
+    }
+    
+    std::string getName() const override {
+        return "MCTS (ConnectFour)";
+    }
+    
+    void setParams(const AlgorithmParams& params) override {
+        params_ = params;
+    }
+    
+    std::unique_ptr<GameState> runAndEvaluate(const GameState& state, int action) override {
+        auto connectfour_state = static_cast<const ConnectFourState&>(state);
+        auto next_state = std::make_unique<ConnectFourState>(connectfour_state);
+        next_state->progress(action);
+        return next_state;
+    }
+};
 
 
 // 알고리즘 팩토리 구현
